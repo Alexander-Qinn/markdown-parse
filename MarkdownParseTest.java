182ef8152d70294e09f918ecda2aa96752bd6568
Alexander-Qinn <97425867+Alexander-Qinn@users.noreply.github.com>
2022-03-11 12:55:20 -0800

    updating my MarkdownParse

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -8,24 +8,9 @@ import java.util.ArrayList;
 
 public class MarkdownParseTest {
     @Test
-    public void addition() {
-        assertEquals(2, 1 + 1);
-    }
-
-    @Test
-    public void multiplication() {
-        assertEquals(10, 5*2);
-    }
-    
-    @Test
     public void testLink() throws IOException {
         String regFile = Files.readString(Path.of("./test-file.md"));
         String[] regLines = regFile.split("\n");
         assertEquals(List.of("https://something.com","some-page.html"), MarkdownParse.getLinks(regLines));
     }
-
-    @Test
-    public void testFail(){
-        assertEquals(1,3);
-    }
-    }
+}
